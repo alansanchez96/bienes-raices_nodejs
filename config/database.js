fa4864cd9449e2f652ab3620970c767c5d604d95
Alan Sanchez <alaansannchezz@gmail.com>
2023-07-13 00:13:01 -0300

    Se instalo dotenv, se configuro el db y se añadio un mysql dockerizado

---
+++ config/database.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import dotenv from './dotenv.js';
+
+
+const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    define: {
+        timestamps: true
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+export default db;
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import auth from './routes/auth.js';
+import db from './config/database.js';
+import dotenv from './config/dotenv.js';
 
 const app = express();
+
+try {
+    await db.authenticate();
+    console.log('conection ok');
+} catch (error) {
+    console.log(error)
+}
 
 app.set('view engine', 'pug')
 app.set('views', './resources/views')
@@ -10,7 +19,7 @@ app.use(express.static('public'))
 app.use('/', auth)
 
 
-const port = 3000;
+const port = process.env.NODE_PORT || 8080;
 
 app.listen(port, () => {
     console.log(`El server esta corriendo desde el puerto: ${port}`)
